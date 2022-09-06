609c6913e2902bb11604bc973704cadef9ec2b3b
willcordeiro <willcordeiro800@gmail.com>
2022-09-06 10:46:26 -0300

    added spring security and fixed some issues

--- userAuthentication/src/main/java/com/example/userauthentication/appuser/AppUserRepository.java
+++ userAuthentication/src/main/java/com/example/userauthentication/appuser/AppUserRepository.java
@@ -2,11 +2,12 @@ package com.example.userauthentication.appuser;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
---
+++ userAuthentication/src/main/java/com/example/userauthentication/registration/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.example.userauthentication.registration;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(path="api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+    
+}
